73de776ce4817b13ef9ad64a6305265a14b9ade9
neferent <neferentlive@gmail.com>
2023-06-30 11:08:24 -0700

    simple matrix selector

---
+++ resources/js/Components/MatrixLabel.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+
+defineProps(['matrix']);
+</script>
+
+<template>
+  <div class="flex flex-row items-center gap-4 p-2 bg-gray-50 radius mb-2">
+    <div class="text-lg font-medium">{{ matrix.name }}</div>
+    <Link href="" class="text-sm underline">edit</Link>
+  </div>
+</template>
